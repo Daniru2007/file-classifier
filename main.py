20d4a34d9367c4725b470e0aa2fa0e3a13379c22
Daniru2007 <daniruayuka@gmail.com>
2022-06-30 19:03:44 +0530

    project finished!

--- main.py
+++ main.py
@@ -4,21 +4,34 @@ import shutil
 src = "src"
 dst = "dst"
 
+file_types = {
+    # images
+    "tiff": "img", "tif": "img", "png": "img", "jpg": "img", "jpeg": "img", "bmp": "img", "svg": "img", "gif": "img", "eps": "img", "raw": "img",
+
+    # code
+    "c": "code", "cpp": "code", "cs":"code", "py": "code", "js": "code", "java": "code", "html": "code", "css": "code", "go": "code", "rs": "code", "r": "code", "ktl": "code", "bf": "code", "b": "code", "asm": "code", "xml": "code", "json": "code", "xaml": "code",
+
+    # documents
+    "doc": "docs", "docx": "docs", "xls": "docs", "xlsx": "docs", "ppt": "docs", "pptx": "docs",
+}
+
+out_dirs = ["img", "code", "doc", "other"]
+
+for ftype in out_dirs:
+    try:
+        os.makedirs(f"{dst}/{ftype}")
+    except FileExistsError:
+        pass
+
 for root, dirs, files in os.walk("src"):
-    print(f"root {root}")
-    for dir in dirs:
-        try:
-            path = root
-            if len(root) > len(src):
-                path = root[len(src):]
-            else:
-                path = ""
-            os.makedirs(f"{dst}/{path}/{dir}")
-            print(f"[FOLDER] creating folder {path}\\{dir}...")
-        except FileExistsError:
-            pass
+    if len(root) > len(src):
+        path = root[len(src):]
+    else:
+        path = ""
 
     for file in files:
-        pass
+        file_type = file.split(".")[-1]
+        print(f"[FILE] {dst}/{file_types.get(file_type, 'other')}/{file}")
+        shutil.copy(f"{root}/{file}", f"{dst}/{file_types.get(file_type, 'other')}/{file}")
 
 
